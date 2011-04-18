091b23e493801f5da49d46a0a475b15990ee62f5
Marc Brinkmann <git@marcbrinkmann.de>
2011-04-18 15:33:12 +0200

    Spell package name without dash.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from distutils.core import setup
 
-setup(name='wsgi-cgi',
+setup(name='wsgicgi',
       version='0.1',
       description='A WSGI wrapper around CGI scripts that allows running legacy CGI applications as WSGI apps. Initially created for running GNU mailman <= 2.1.',
       author='Marc Brinkmann',
