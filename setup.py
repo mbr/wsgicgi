b4680535b3e204530b0ec0244949ac3e4e0f415d
Marc Brinkmann <git@marcbrinkmann.de>
2011-04-18 15:29:15 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from distutils.core import setup
+
+setup(name='wsgi-cgi',
+      version='0.1',
+      description='A WSGI wrapper around CGI scripts that allows running legacy CGI applications as WSGI apps. Initially created for running GNU mailman <= 2.1.',
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/wsgi-cgi',
+      #packages=[''],
+     )
